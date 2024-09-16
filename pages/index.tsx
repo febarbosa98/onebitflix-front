bcb7d8e7f61096c4c3412d1f7188758440c0689f
Fernando Barbosa <fernando_fe1@hotmail.com>
2024-09-16 16:10:52 -0300

    criação do header da pagina homeNoAuth - header não autorizado

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,22 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import Head from "next/head";
-import styles from '../styles/HomeNoAuth.module.scss'
+import styles from "../styles/homeNoAuth.module.scss" 
+import HeaderNoAuth from "@/src/components/homeNoAuth/headerNoAuth";
 
 const HomeNotAuth = function () {
   return (
 		<>
 			<Head>
-        <title>Home</title>
-      </Head>
-			<main></main>
+				<title>Onebitflix</title>
+				<link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
+				<meta property="og: title" content="Onebitflix" key='title'/>
+				<meta name="description" content="tenha acesso aos melhores conteudos de programação simples e facil"/>
+			</Head>
+			<main>
+				<HeaderNoAuth/>
+			</main>
 		</>
-  );
+  )
 };
 
 export default HomeNotAuth;
