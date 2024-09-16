0a383ce93ef46f4af26fad4a9c7d4b69dbb3d0aa
Fernando Barbosa <fernando_fe1@hotmail.com>
2024-09-16 20:44:50 -0300

    criação dos slide, componete pai e filho. conexão com backend

---
+++ src/components/common/slideCard/index.tsx
@@ -0,0 +1,23 @@
+/* eslint-disable @next/next/no-img-element */
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+
+import styles from "./styles.module.scss"
+import { CourseType } from "@/src/services/courseService";
+
+interface props {
+    course: CourseType
+}
+
+const SlideCard = function ({course}: props) {
+    return (
+        <>
+            <div className={styles.slide}>
+                <img src={`${process.env.NEXT_PUBLIC_BASEURL}/${course.thumbnailUrl}`} alt={course.name} className={styles.slideImg}/>
+                <p className={styles.slideTitle}>{course.name}</p>
+                <p className={styles.slideDescription}>{course.synopsis}</p>
+            </div>
+        </>
+
+    )
+};
+export default SlideCard
