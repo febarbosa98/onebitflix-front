c88d4b5d487ff1d84956253b65489988f9330331
Fernando Barbosa <fernando_fe1@hotmail.com>
2024-11-04 16:27:38 -0300

    criação dos elementos do header logado

--- pages/register.tsx
+++ pages/register.tsx
@@ -5,7 +5,7 @@ import styles from "../styles/registerLogin.module.scss";
 import Head from "next/head";
 import { Form, FormGroup, Label, Container, Button, Input } from "reactstrap";
 import Footer from "@/src/components/common/footer";
-import { FormEvent, useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
 import authService from "@/src/services/authService";
 import { useRouter } from "next/router";
 import ToastComponent from "@/src/components/common/toast";
@@ -14,6 +14,13 @@ const Register = function () {
   const router = useRouter()
   const [toastIsOpen, setToastIsOpen] = useState(false);
   const [toastMessage, setToastMessage] = useState("");
+
+  useEffect(()=>{
+    if (sessionStorage.getItem("onebitflix-token")) {
+        router.push("/home")
+    }
+}, [])
+
   const handleRegister = async (event: FormEvent<HTMLFormElement>) =>{
     event.preventDefault();
 
