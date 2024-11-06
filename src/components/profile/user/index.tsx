5f3ce19e017b4cf3a57bdbd0eb58fc5ecf8cc69a
Fernando Barbosa <fernando_fe1@hotmail.com>
2024-11-06 15:51:55 -0300

    criação dos elemtos da pagina de informaçoes do usuario do seu estilo visual e tambem da sua responsividade - profile(usuario-visual)

---
+++ src/components/profile/user/index.tsx
@@ -0,0 +1,44 @@
+/* eslint-disable @next/next/no-img-element */
+import { Button, Form, FormGroup, Input, Label } from "reactstrap"
+import styles from "../../../../styles/profile.module.scss"
+
+const UserForm = function () {
+    return (
+        <>
+        <Form className={styles.form}> 
+            <div className={styles.formName}>
+                <p className={styles.nameAbbreviation}>NT</p>
+                <p className={styles.userName}>NAME TEST</p>
+            </div>
+            <div className={styles.memberTime}>
+                <img className={styles.memberTimeImg} src="/profile/iconUserAccount.svg" alt="iconProfile" />
+                <p className={styles.memberText}>Membro desde <br /> 20 de Abril de 2020</p>
+            </div>
+            <hr />
+           <div className={styles.inputFlexDiv}>
+           <FormGroup>
+                <Label for="firstName" className={styles.label}>NOME</Label>
+                <Input name="firstName" type="text" id="firstName" placeholder="Qual o seu primeiro nome?" required maxLength={20} className={styles.inputFlex} value={"Name"}></Input>
+            </FormGroup>
+            <FormGroup>
+                <Label for="lastName" className={styles.label}>SOBRENOME</Label>
+                <Input name="lastName" type="text" id="lastName" placeholder="Qual o seu ultimo nome?" required maxLength={20} className={styles.inputFlex} value={"Test"}></Input>
+            </FormGroup>
+           </div>
+           <div className={styles.inputNormalDiv}>
+           <FormGroup>
+                <Label for="phone" className={styles.label}>WHATSAPP / TELEGRAM</Label>
+                <Input name="phone" type="tel" id="phone" placeholder="(xx) 9xxxx-xxxx" required className={styles.input} value={"+55 (21) 99999-9999"}></Input>
+            </FormGroup>
+            <FormGroup>
+                <Label for="email" className={styles.label}>E-MAIL</Label>
+                <Input name="email" type="email" id="email" placeholder="Coloque o seu email" required className={styles.input} value={"teste@email.com"}></Input>
+            </FormGroup>
+            <Button className={styles.formBtn} outline type="submit">Salvar Alterações</Button>
+           </div>
+        </Form>
+        </>
+    )
+}
+
+export default UserForm
