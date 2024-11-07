ef350b03b916752a0e2ace516af93b517994d98d
Fernando Barbosa <fernando_fe1@hotmail.com>
2024-11-07 01:05:10 -0300

    Conexão do backend relacionado aos dados do usuário. Criação de um serviço e também de um método e também de toda a parte de conexão com o front - Conexão backend dados do usuário

--- src/components/profile/user/index.tsx
+++ src/components/profile/user/index.tsx
@@ -1,42 +1,97 @@
 /* eslint-disable @next/next/no-img-element */
 import { Button, Form, FormGroup, Input, Label } from "reactstrap"
 import styles from "../../../../styles/profile.module.scss"
+import { FormEvent, useEffect, useState } from "react"
+import profileService from "@/src/services/profileService"
+import ToastComponent from "../../common/toast"
+import { useRouter } from "next/router"
 
 const UserForm = function () {
+
+const router = useRouter()
+const [color, setColor] = useState('')
+const [toastIsOpen, setToastIsOpen]= useState(false)
+const [errorMessage, setErrorMessage]= useState('')
+const [firstName, setFirstName] = useState("")
+const [lastName, setLastName] = useState("")
+const [phone, setPhone] = useState("")
+const [email, setEmail] = useState("")
+const [initialEmail, setInitialEmail] = useState(email)
+const [created_at, setCreated_at] = useState("")
+const date = new Date(created_at)
+const month = date.toLocaleDateString("default", {month: "long"})
+
+useEffect(() => {
+	profileService.fetchCurrent().then((user) => {
+	    setFirstName(user.firstName);
+        setLastName(user.lastName);
+        setPhone(user.phone);
+        setEmail(user.email);
+        setInitialEmail(user.email);
+        setCreated_at(user.createdAt);
+	});
+}, []);
+
+const handleUserUpdate = async function (event: FormEvent<HTMLFormElement>){
+    event.preventDefault()
+    const res = await profileService.userUpdate({
+        firstName, lastName, phone, email, created_at
+    })
+
+    if(res === 200){
+        setToastIsOpen(true)
+        setErrorMessage("informações alterada com sucesso!")
+        setColor("bg-success")
+        setTimeout(()=>setToastIsOpen(false), 1000 * 3)
+        if(email != initialEmail) {
+            sessionStorage.clear()
+            router.push('/')
+        }
+    } else{
+        setToastIsOpen(true)
+        setErrorMessage("Você não pode mudar para esse email")
+        setColor("bg-danger")
+        setTimeout(()=>setToastIsOpen(false), 1000 * 3)
+    }
+}
+
     return (
         <>
-        <Form className={styles.form}> 
+        <Form onSubmit={handleUserUpdate} className={styles.form}> 
             <div className={styles.formName}>
-                <p className={styles.nameAbbreviation}>NT</p>
-                <p className={styles.userName}>NAME TEST</p>
+                <p className={styles.nameAbbreviation}>
+                    {firstName.slice(0, 1)}{lastName.slice(0,1)}
+                </p>
+                <p className={styles.userName}>{`${firstName} ${lastName}`}</p>
             </div>
             <div className={styles.memberTime}>
                 <img className={styles.memberTimeImg} src="/profile/iconUserAccount.svg" alt="iconProfile" />
-                <p className={styles.memberText}>Membro desde <br /> 20 de Abril de 2020</p>
+                <p className={styles.memberText}>Membro desde <br /> {`${date.getDate()} de ${month} de ${date.getFullYear()}`}</p>
             </div>
             <hr />
            <div className={styles.inputFlexDiv}>
            <FormGroup>
                 <Label for="firstName" className={styles.label}>NOME</Label>
-                <Input name="firstName" type="text" id="firstName" placeholder="Qual o seu primeiro nome?" required maxLength={20} className={styles.inputFlex} value={"Name"}></Input>
+                <Input name="firstName" type="text" id="firstName" placeholder="Qual o seu primeiro nome?" required maxLength={20} className={styles.inputFlex} value={firstName} onChange={(event) => {setFirstName(event.target.value)}}></Input>
             </FormGroup>
             <FormGroup>
                 <Label for="lastName" className={styles.label}>SOBRENOME</Label>
-                <Input name="lastName" type="text" id="lastName" placeholder="Qual o seu ultimo nome?" required maxLength={20} className={styles.inputFlex} value={"Test"}></Input>
+                <Input name="lastName" type="text" id="lastName" placeholder="Qual o seu ultimo nome?" required maxLength={20} className={styles.inputFlex} value={lastName} onChange={(event) => {setLastName(event.target.value)}} ></Input>
             </FormGroup>
            </div>
            <div className={styles.inputNormalDiv}>
            <FormGroup>
                 <Label for="phone" className={styles.label}>WHATSAPP / TELEGRAM</Label>
-                <Input name="phone" type="tel" id="phone" placeholder="(xx) 9xxxx-xxxx" required className={styles.input} value={"+55 (21) 99999-9999"}></Input>
+                <Input name="phone" type="tel" id="phone" placeholder="(xx) 9xxxx-xxxx" required className={styles.input} value={phone} onChange={(event) => {setPhone(event.target.value)}}></Input>
             </FormGroup>
             <FormGroup>
                 <Label for="email" className={styles.label}>E-MAIL</Label>
-                <Input name="email" type="email" id="email" placeholder="Coloque o seu email" required className={styles.input} value={"teste@email.com"}></Input>
+                <Input name="email" type="email" id="email" placeholder="Coloque o seu email" required className={styles.input} value={email} onChange={(event) => {setEmail(event.target.value)}}></Input>
             </FormGroup>
             <Button className={styles.formBtn} outline type="submit">Salvar Alterações</Button>
            </div>
         </Form>
+        <ToastComponent color={color} isOpen={toastIsOpen} message={errorMessage} />
         </>
     )
 }
